196a053fe1374c886555d6ed2d2f095c8cd6c3c9
Pratik Desai <pratikdesai617@gmail.com>
2019-05-01 21:20:10 +0530

    init commit

---
+++ resources/views/listings/create.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="row justify-content-center">
+    <div class="col-md-8">
+        <div class="card">
+            <div class="card-header">Create List <a href="{{ route('dashboard') }}" class="btn btn-default btn-sm float-right">Go Back</a></div>
+
+            <div class="card-body">
+                @if (session('status'))
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
+                @endif
+
+				{!! Form::open(['action' => 'ListingsController@store', 'method' => 'POST']) !!}
+				    {{ Form::bsText('name', '', ['placeholder' => 'Company Name']) }}
+                    {{ Form::bsText('website', '', ['placeholder' => 'Company Website']) }}
+				    {{ Form::bsText('email', '', ['placeholder' => 'Contact Email']) }}
+				    {{ Form::bsText('phone', '', ['placeholder' => 'Contact Phone']) }}
+				    {{ Form::bsText('address', '', ['placeholder' => 'Business Address']) }}
+				    {{ Form::bsTextArea('bio', '', ['placeholder' => 'About this business']) }}
+				    {{ Form::bsSubmit('submit') }}
+				{!! Form::close() !!}
+
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
